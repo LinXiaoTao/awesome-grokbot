fe98f7586a0d3462f49d3c8952482cf5365a1aff
leo <linxiaotao1993@gmail.com>
2026-08-30 23:01:15 +0800

    Update Google Tag Manager script loading strategy in layout.tsx for improved performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,9 +62,9 @@ export default function RootLayout({
       <head>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="gtag-init" strategy="afterInteractive">
+        <Script id="gtag-init" strategy="beforeInteractive">
           {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_ID}');`}
         </Script>
         <Script
